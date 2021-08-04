bc804d0e85ca1550c9a263a49e86dcab065cbaf3
Nguyen Anh Tuan <33346539+greny98@users.noreply.github.com>
2021-08-04 14:52:02 +0700

    Add argparser to cli

--- main.py
+++ main.py
@@ -1,8 +1,18 @@
 from one_shot_model import siamese, encoding
 from libs import *
 import data_generator
+import argparse
 
 if __name__ == '__main__':
+    # Handle arg
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--epochs', type=int, default=50)
+    parser.add_argument('--lr', type=float, default=0.0005)
+    args = vars(parser.parse_args())
+    epochs = args['epochs']
+    lr = args['lr']
+
+    # Read data.csv
     df = pd.read_csv('data/data.csv')
     ds_train, ds_test = data_generator.create_ds(df)
     input1 = L.Input(shape=(224, 224, 3,))
@@ -10,9 +20,12 @@ if __name__ == '__main__':
     output = siamese(input1, input2)
     model = models.Model(inputs=[input1, input2], outputs=output)
     model.summary()
-    model.compile(loss=losses.binary_crossentropy, optimizer='adam', metrics=['acc'])
+    model.compile(
+        loss=losses.binary_crossentropy,
+        optimizer=optimizers.Adam(learning_rate=lr),
+        metrics=['acc'])
     model.fit(
         ds_train,
         validation_data=ds_test,
-        epochs=3
+        epochs=epochs
     )
